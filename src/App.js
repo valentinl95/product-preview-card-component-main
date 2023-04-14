c7285d83c076ee26ec40cacf1eda8fc02dc0f3da
Valentin Lukyanets <valikluks95@gmail.com>
2023-04-14 18:54:02 +0300

    Developed Product Preview Card Design Demo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="image-wrapper"/>
+        <div className="content">
+          <div className="main-content">
+            <p className="montserrat title grey">PERFUME</p>
+            <p className="fraunces bold name">Gabrielle Essence Eau De Parfum</p>
+            <p className="montserrat text grey">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+            <div className="pricing">
+              <div className="actual-price fraunces">$149.99</div>
+              <div className="base-price grey strike-through montserrat">$169.99</div>
+            </div>
+          </div>
+          <div className="add-to-cart montserrat">
+            <img src="/icon-cart.svg" alt="" />
+            <p>Add to Cart</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
